80001f5bc29ebaa2cad3dc683098b79b7e7e3e1e
Moxy <keanu.knighton@gmail.com>
2023-02-27 18:46:42 +0000

    Questions function and markddown print response

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# ReadMe-Generator
+# test
+![Github license](https://img.shields.io/badge/license-MIT-blue.svg)
+
+### Description
+
+test
+
+### Table of Contents
+
+* [Installation](#installation)
+
+
+* [Usage](#usage)
+
+
+* [License](#license)
+
+
+* [Contributing](#contributing)
+
+
+* [Questions](#questions)
+
+
+## Installation
+
+To install necessary dependencies, run the following command:
+
+```
+npm i
+
+```
+
+## Usage
+
+
+
+## License
+
+## Contributing
+
+test
+
+## Tests
+
+To run test, run following command:
+
+```
+npm test
+
+```
+
+### Questions
+
+For any inquiries or questions please contact me directly at test.
+
+You can findd more of my work  at [undefined](https://github.com/undefined).
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,76 @@
 // function to generate markdown for README
+function renderLicensebadge(license) {
+  if(license !== "none") {
+    return `![Github license](https://img.shields.io/badge/license-${license}-blue.svg)`
+  }
+}
+
+const renderLicenseLink = (license) => {
+  if(license !== "None") {
+    return `\n* [License](#license)\n`
+  }
+  return '';
+}
+
 function generateMarkdown(data) {
   return `# ${data.title}
+${renderLicensebadge(data.license)}
+
+### Description
+
+${data.description}
+
+### Table of Contents
+
+* [Installation](#installation)
+
+
+* [Usage](#usage)
+
+${renderLicenseLink(data.license)}
+
+* [Contributing](#contributing)
+
+
+* [Questions](#questions)
+
+
+## Installation
+
+To install necessary dependencies, run the following command:
+
+\`\`\`
+${data.installation}
+
+\`\`\`
+
+## Usage
+
+${data.usage}
+
+## License
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+To run test, run following command:
+
+\`\`\`
+${data.test}
+
+\`\`\`
+
+### Questions
+
+For any inquiries or questions please contact me directly at ${data.email}.
+
+You can findd more of my work  at [${data.github}](https://github.com/${data.github}).
 
 `;
 }
 
 module.exports = generateMarkdown;
+
